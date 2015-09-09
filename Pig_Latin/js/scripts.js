49ef31890e8ae8ff4d8abecf5564925cfebe75c0
Kevin Tokheim & alexandra brown <student@epicodus.com>
2015-09-09 10:17:21 -0700

    pass first test again

--- Pig_Latin/js/scripts.js
+++ Pig_Latin/js/scripts.js
@@ -1,4 +1,11 @@
 var pigLatin = function(word) {
-  var newWord = word + 'ay';
+
+  // var vowels  = ['a', 'e', 'i', 'o', 'u'];
+
+  var firstLetter = word[0];
+
+  if (firstLetter == 'a' || 'e' || 'i' || 'o' || 'u') {
+    var newWord = word + 'ay';
+  }
   return newWord;
 };
